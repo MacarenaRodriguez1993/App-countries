5e24afc6104ae6e29ed458b6ff99463a3dfb9ab4
Macarena Rodriguez <maca.b.rodriguez@gmail.com>
2023-01-15 11:51:06 -0300

    va

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const DB_PORT = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
+  server.listen(DB_PORT, () => {
     console.log(`%s listening at${DB_PORT}`); // eslint-disable-line no-console
   });
 });
